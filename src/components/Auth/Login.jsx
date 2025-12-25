d917c1a579032cac5d939b78e3068a811ce5ed04
PC Home <gauravsoni2430@gmail.com>
2025-12-25 12:48:13 +0530

    version 1.0.0

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Login = () => {
+    const [Email , setEmail] = useState("");
+    const [Password , setPassword] = useState("");
+
+    
+    const SubmitHandler = (e) => {
+        e.preventDefault();
+        console.log("Form Submitted");
+    }
+
+    return (
+        <div className="h-screen w-screen justify-center items-center flex">
+            <div className="login-page border-2 border-emerald-500 rounded-2xl p-20">
+                <form
+                onSubmit={(e) => {
+                    SubmitHandler(e);
+                } 
+            }
+                className="m-12 flex flex-col gap-2 justify-center items-center" action="">
+                    <input 
+                    onChange={(e)=>{
+                        console.log(e.target.value);
+                        setEmail(e.target.value);
+                    }}
+                    required className="text-white border-2 border-emerald-500 rounded-full bg-transparent placeholder:text-gray-400 placeholder:bg-transparent p-3" type="email" placeholder="Enter Email" name="" id=""
+                    />
+
+                    <input 
+                    onChange={(e)=>{
+                        console.log(e.target.value);
+                        setPassword(e.target.value);
+                    }}
+
+                    required className="text-white border-2 border-emerald-500 rounded-full bg-transparent placeholder:text-gray-400 p-3 placeholder:bg-transparent" type="password" placeholder="Enter Password" name="" id="" 
+                    />
+
+                    <button className="emr-button w-[35%] text-white rounded-full p-2" type="submit">Login
+                    </button>
+                </form>
+            </div>
+        </div>
+        
+    );
+}
+
+export default Login;
